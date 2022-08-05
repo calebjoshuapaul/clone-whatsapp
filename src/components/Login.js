7e3737c605f1c5dcb40b080f8bcb493fb848f9c1
Caleb <calebjoshuapaul@gmail.com>
2022-08-06 01:44:30 +0530

    update

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,7 @@ import { useStateValue } from "../StateProvider";
 import { actionTypes } from "../reducer";
 
 function Login() {
-  const [{}, dispatch] = useStateValue();
+  const [, dispatch] = useStateValue();
 
   const signIn = () => {
     auth
